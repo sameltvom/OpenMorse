9ebf3d51703253d3b9f5112e4c0be40aea75004b
Samuel Skanberg <samuel_skanberg@yahoo.se>
2010-10-16 20:19:57 +0200

    Added gitignore

--- src/com/example/morse/SimpleMorse.java
+++ src/com/example/morse/SimpleMorse.java
@@ -2,6 +2,7 @@ package com.example.morse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,6 +10,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class SimpleMorse extends Activity {
+	/* When the morse tone started */
+	private long start;
+	
+	/* Have we pressed but not released? */
+	private boolean isPressed = false;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,25 +24,33 @@ public class SimpleMorse extends Activity {
         
         ImageView image = (ImageView)findViewById(R.id.button_image);
         image.setOnTouchListener(mOnTouchImageListener);
+        image.setOnClickListener(mOnImageClickListener);
         
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(mOnButtonClickListener);
+        //Button button = (Button)findViewById(R.id.button);
+        //button.setOnClickListener(mOnButtonClickListener);
     }
     
     
     private View.OnTouchListener mOnTouchImageListener = new View.OnTouchListener() {
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
-			Log.d("SimpleMorse", "touch");
+			if (!isPressed) {
+				start = SystemClock.uptimeMillis();
+				isPressed = true;
+			}
 			return false;
 		}
 	};
 	
-	private View.OnClickListener mOnButtonClickListener = new View.OnClickListener() {
+	private View.OnClickListener mOnImageClickListener = new View.OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
-			Log.d("SimpleMorse", "button click");
+			long end = SystemClock.uptimeMillis();
+			long diff = end-start;
+			Log.d("SimpleMorse", "time: "+diff);
+			
+			isPressed = false;
 		}
 	};
 }
